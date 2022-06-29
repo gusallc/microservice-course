e6b288a190dbdcfa6bd9767e47de79841b43d3d2
Gustavo Chiclla <gustavohoper@gmail.com>
2022-06-29 14:12:53 -0500

    Configuración final de reciliense4j

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -1,25 +1,25 @@
 package academy.digitallab.store.shopping.client;
 
 import academy.digitallab.store.shopping.model.Customer;
-//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", path = "/customers", fallback = CustomerFallbackFactory.class)
+@FeignClient(name = "customer-service", path = "/customers")
 public interface CustomerClient {
 
-//    @CircuitBreaker(name = "customerCB", fallbackMethod = "getCustomerFallbackMethod")
+    @CircuitBreaker(name = "customerCB", fallbackMethod = "getCustomerFallbackMethod")
     @GetMapping(value = "/{id}")
     ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
 
-//    default ResponseEntity<Customer> getCustomerFallbackMethod(Exception e) {
-//        Customer customer = Customer.builder()
-//                .firstName("none")
-//                .lastName("none")
-//                .email("none")
-//                .photoUrl("none").build();
-//        return ResponseEntity.ok(customer);
-//    }
+    default ResponseEntity<Customer> getCustomerFallbackMethod(Exception exc) {
+        Customer customer = Customer.builder()
+                .firstName(exc.getMessage())
+                .lastName("none")
+                .email("none")
+                .photoUrl("none").build();
+        return ResponseEntity.ok(customer);
+    }
 }
